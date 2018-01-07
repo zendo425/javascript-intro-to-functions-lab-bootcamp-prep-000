7dc66191767bfd97bc5d3145d2c1992a48a42926
Demetrious Young <zendo425@gmail.com>
2018-01-07 06:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 function shout(string){
+  
   return string.toUpperCase();
 }
 
 function whisper(string){
+  
   return string.toLowerCase();
 }
 
 function logShout(string){
+  
   console.log('HELLO');
+  
   return string.toUpperCase();
 }
 
 function logWhisper(string){
+  
   console.log("hello");
+  
   return string.toLowerCase();
 }
 
