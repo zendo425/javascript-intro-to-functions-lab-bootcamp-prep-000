0d9f991f6ed05c21b0ee096a45c28255d4fb87d3
Demetrious Young <zendo425@gmail.com>
2018-01-07 06:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function sayHiToGrandma(string) {
     else if (string === string.toUpperCase())
        return ("YES INDEED!");
  
-    else if (string === "I love you, Grandma.")
+    else if (string === ("I love you, Grandma."))
      return ("I love you, too.");
  
