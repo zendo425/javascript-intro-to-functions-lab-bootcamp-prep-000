9f0c4dadef0aa36dd35f53384db83c4412f1f450
Demetrious Young <zendo425@gmail.com>
2018-01-07 06:33:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log('HELLO');
+  return string.toUpperCase();
+}
+
+function logWhisper(string){
+  console.log("hello");
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string){
+  return("I can't hear you!");
+  return string.toLowererCase();
+}
+
+function sayHiToGrandma(string){
+  return("YES INDEED!");
+  return string.toUpperCase();
+}
+
+function sayHiToGrandma(string){
+  return("I can't hear you!");
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string){
+  return("I love you, Grandma!");
+  return string.toUpperCase();
+}
